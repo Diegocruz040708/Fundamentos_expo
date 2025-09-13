33da8795f79b3a4b144e40eab5fcc60dd5929c91
Diegocruz040708 <cruzdiego040708@gmail.com>
2025-09-13 10:07:11 -0600

    "4. Creando botones"

--- src/pages/Butones.js
+++ src/pages/Butones.js
@@ -1,9 +1,17 @@
-import { Text, View, StyleSheet, Platform, StatusBar } from 'react-native';
+import { Text, View, StyleSheet, Platform, StatusBar, TouchableOpacity } from 'react-native';
 
 export default function Butones() {
   return (
     <View style={style.mainS}>
-        <Text>Botones</Text>
+        <Text style={style.h1} >Botones</Text>
+        {/* Botón 1 */}
+        <TouchableOpacity style={style.botonBasico}>
+            <Text style={style.text}>Botón 1</Text>
+        </TouchableOpacity>
+        {/* Botón 2 */}
+        <TouchableOpacity style={style.botonIcono}>
+            <Text style={style.text}>Botón Icono</Text>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -14,5 +22,21 @@ const style = StyleSheet.create({
         backgroundColor: '#00b3ff',
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
         padding: 16,
-    }
+    },
+    h1: {
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+    botonBasico: {
+        backgroundColor: '#fff',
+        padding: 12,
+        borderRadius: 10,
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    text: {
+        color: '#000',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
 });
