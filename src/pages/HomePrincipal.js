f764196424a0da37e82ab60617103a2c01681990
Diegocruz040708 <cruzdiego040708@gmail.com>
2025-09-14 00:40:35 -0600

    "10. Haciendo uso de ScrollView"

--- src/pages/HomePrincipal.js
+++ src/pages/HomePrincipal.js
@@ -1,17 +1,21 @@
-import { Text, View, StyleSheet, Platform, StatusBar, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Platform, StatusBar, TouchableOpacity, ScrollViewBase, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import CardBasico from '../components/CardBasico';
 import Cardimg from '../components/Cardimg';
 import CardV2 from '../components/CardV2';
+import CardV3 from '../components/CardV3';
 
 export default function HomePrincipal() {
   return (
+    <ScrollView>
     <View style={style.mainS}>
         <Text style={style.title} >Pagina Principal</Text>
         <CardBasico />
         <Cardimg />
         <CardV2/>
+        <CardV3/>
     </View>
+    </ScrollView>
   );
 }
 
